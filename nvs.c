19e62c6622c0138c4a9e2818bfa6f423066fdb9f
Kpyush <peeyush.ei@gmail.com>
2024-06-09 14:21:54 +0530

    initial commit

---
+++ nvs.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <nvs_flash.h>
+#include <esp_http_client.h>
+#include <esp_http_server.h>
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASS "password"
+#define SENSOR_INTERVAL "sensor_interval"
+#define UPLOAD_URL "upload_url"
+
+static esp_err_t nvs_set_config(int interval, const char* url)
+{
+    nvs_handle_t nvs_handle;
+    esp_err_t err;
+
+    err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
+        return err;
+    }
+
+    err = nvs_set_i32(nvs_handle, SENSOR_INTERVAL, interval);
+    if (err != ESP_OK) {
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    err = nvs_set_str(nvs_handle, UPLOAD_URL, url);
+    if (err != ESP_OK) {
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    err = nvs_commit(nvs_handle);
+    if (err != ESP_OK) {
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    nvs_close(nvs_handle);
+    return ESP_OK;
+}
+
+static esp_err_t nvs_get_config(char* ssid, char* password)
+{
+    nvs_handle_t nvs_handle;
+    esp_err_t err;
+
+    err = nvs_open("storage", NVS_READONLY, &nvs_handle);
+    if (err != ESP_OK) {
+        return err;
+    }
+
+    size_t ssid_len = 32;
+    size_t password_len = 64;
+    err = nvs_get_str(nvs_handle, WIFI_SSID, ssid, &ssid_len);
+    if (err != ESP_OK) {
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    err = nvs_get_str(nvs_handle, WIFI_PASS, password, &password_len);
+    if (err != ESP_OK) {
+        nvs_close(nvs_handle);
+        return err;
+    }
+
+    nvs_close(nvs_handle);
+    return ESP_OK;
+}
+
+void app_main(void)
+{
+    char ssid[32] = {0};
+    char password[64] = {0};
+    char url[128] = {0};
+    size_t url_len = sizeof(url);
+
+    sp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_wifi_init(&WIFI_INIT_CONFIG_DEFAULT()));
+
+     if (nvs_get_wifi_config(ssid, password) == ESP_OK) 
+}
